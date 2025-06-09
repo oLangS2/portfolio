e3f3f267bbb9e48f304f32726928fae83a5adc2f
OlangS2 <guswjd8389@gmail.com>
2025-06-09 15:41:35 +0900

    섹션 마진값 수정

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 const footer = () => {
   return (
-    <div className='section' id="footer">
-        <div className="section_wrap footer_wrap">
-          <div className="title">
-            봐주셔서 감사합니다 :)<br />
-            회사와 팀에 가치를 더하는 개발자가 되겠습니다.
+    <div id="footer">
+        <div className="footer_wrap">
+          <div className="box">
+            <div className="title">
+              봐주셔서 감사합니다 :)<br />
+              회사와 팀에 가치를 더하는 개발자가 되겠습니다.
+            </div>
+            <a href="https://infrequent-accordion-3ab.notion.site/Front-End-1b3049798a358099bfefe87eb8f8d2bb?pvs=74" target="_blank" rel="noopener noreferrer" className='notion_link'>
+              Notion
+            </a>
+            <a href="https://github.com/oLangS2" target="_blank" rel="noopener noreferrer" className='git_link'>
+              Github
+            </a>
           </div>
-          <a href="https://infrequent-accordion-3ab.notion.site/Front-End-1b3049798a358099bfefe87eb8f8d2bb?pvs=74" target="_blank" rel="noopener noreferrer" className='notion_link'>
-            Notion
-          </a>
-          <a href="https://github.com/oLangS2" target="_blank" rel="noopener noreferrer" className='git_link'>
-            Github
-          </a>
         </div>
     </div>
   )
