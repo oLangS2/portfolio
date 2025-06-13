cbd05d581e2f3357b994af2167b7f382032684f3
OlangS2 <guswjd8389@gmail.com>
2025-06-13 15:40:11 +0900

    푸터 텍스트 수정

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -7,8 +7,8 @@ const footer = () => {
           <div className="box">
             <div className="title">
               봐주셔서 감사합니다 :)<br />
-              회사와 팀에 가치를 더하는 개발자가 되겠습니다.
             </div>
+            <p>회사와 팀에 가치를 더하는 개발자가 되겠습니다.</p>
             <a href="https://infrequent-accordion-3ab.notion.site/Front-End-1b3049798a358099bfefe87eb8f8d2bb?pvs=74" target="_blank" rel="noopener noreferrer" className='notion_link'>
               Notion
             </a>
