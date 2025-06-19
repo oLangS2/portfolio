35ada64b5a319ff4bf14c64a3b4d3a4f9d94d48e
OlangS2 <guswjd8389@gmail.com>
2025-06-19 21:15:41 +0900

    프로젝트 스타일 수정 완료

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { AiOutlinePlusCircle } from "react-icons/ai";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const ProjectCard = ({ data, onClick }) => {
   return (
     <div className="card" onClick={onClick}>
       <div className="desc">
+        <div className='icon'>{data.icon}</div>
         <h3>{data.company}</h3>
         <span>{data.period}</span>
         <p>{data.title}</p>
       </div>
       <button className="detail_more">
-        <AiOutlinePlusCircle size={25}/>
+        자세히 보기
       </button>
     </div>
   );
