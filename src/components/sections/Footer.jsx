78c321247c4ac956cc1e38ed4bf5a3ce90d27fe8
OlangS2 <guswjd8389@gmail.com>
2025-06-22 16:09:51 +0900

    푸터 멘트 수정

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -8,7 +8,11 @@ const footer = () => {
             <div className="title">
               봐주셔서 감사합니다 :)<br />
             </div>
-            <p>회사와 팀에 가치를 더하는 개발자가 되겠습니다.</p>
+            <p>
+              {/* 낯선 기술에도 두려움 없이 도전하며 끊임없이 성장하겠습니다. <br /> */}
+              효율적인 사용자 경험을 만들어내는 프론트엔드 개발자가 되어 <br />
+              회사와 팀에 실질적인 가치를 더하겠습니다.
+            </p>
             <a href="https://infrequent-accordion-3ab.notion.site/Front-End-1b3049798a358099bfefe87eb8f8d2bb?pvs=74" target="_blank" rel="noopener noreferrer" className='notion_link'>
               Notion
             </a>
