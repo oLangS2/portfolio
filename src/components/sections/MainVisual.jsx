df579c83d8b235d4ac856341d27ff50237f8aba8
OlangS2 <guswjd8389@gmail.com>
2026-06-13 15:00:13 +0900

    feat: 메인비주얼 수정

--- src/components/sections/MainVisual.jsx
+++ src/components/sections/MainVisual.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import "./MainVisual.scss"
+import { useTyping } from '../../hooks/useTyping'
+
+const phrases = [
+  '퍼블리싱 완성도와 React 개발, 둘 다 합니다.',
+  '픽셀 단위로 구현하고, 구조 단위로 설계합니다.',
+  '일정 안에 확실하게 구현합니다.',
+]
 
 const MainVisual = () => {
+  const typingText = useTyping(phrases)
+
   return (
     <div className='main-visual'>
       <div className="main-visual__inner">
@@ -10,11 +19,17 @@ const MainVisual = () => {
           <span>웹 퍼블리셔</span>
           <span>프론트엔드 개발자</span>
         </div>
-        <div className='main-visual__title'>마크업을 정확하게,<br />컴포넌트를 유연하게.</div>
-        <button className="main-visual__btn">
+        <div className='main-visual__title'>마크업을 정확하게,<br />컴포넌트를 <span className="main-visual__highlight">유연하게.</span></div>
+       
+        <p className="main-visual__typing">
+          {typingText}
+          <span className="main-visual__caret" />
+        </p>
+{/* 
+        <a href='#projects' className="main-visual__btn">
           프로젝트 보기
           <span className="main-visual__btn-icon">↓</span>
-        </button>
+        </a> */}
       </div>
     </div>
   )
